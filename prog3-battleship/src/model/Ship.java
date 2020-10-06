566bca32ec385025b7a735e686ae91a237aa1b6e
Rubén <Rubn@DESKTOP-6P88VKD>
2020-10-06 15:20:50 +0200

    Ship Structure

--- prog3-battleship/src/model/Ship.java
+++ prog3-battleship/src/model/Ship.java
@@ -1,5 +1,68 @@
 package model;
 
-class Ship {
+import java.util.*;
 
+public class Ship {
+	private static final int BOUNDING_SQUARE_SIZE;
+	private static final int HIT_VALUE;
+	private static final int CRAFT_VALUE;
+	private char symbol;
+	private String name;
+	private int[][] shape;
+	
+	public Ship(Orientation o, char s, String n) {
+		
+	}
+	
+	public Coordinate getPosition() {
+		
+	}
+	
+	public void setPosition(Coordinate position) {
+		
+	}
+	
+	public String getName() {
+		
+	}
+	
+	public Orientation getOrientation() {
+		
+	}
+	
+	public char getSymbol() {
+		
+	}
+	
+	public int[][] getShape(){
+		
+	}
+	
+	public int getShapeIndex(Coordinate c) {
+		
+	}
+	
+	public Set<Coordinate> getAbsolutePositions(Coordinate c){
+		
+	}
+	
+	public Set<Coordinate> getAbsolutePositions(){
+		
+	}
+	
+	public boolean hit(Coordinate c) {
+		
+	}
+	
+	public boolean isShotDown() {
+		
+	}
+	
+	public boolean isHit(Coordinate c) {
+		
+	}
+	
+	public String toString() {
+		
+	}
 }
