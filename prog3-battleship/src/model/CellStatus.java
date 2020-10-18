8d085b4b64f8aea7b2569fe56e802a9018d5bfdc
ruben <ruben@Ruben-Laptop>
2020-10-18 16:51:05 +0200

    CellStatus.java v1.0J

--- prog3-battleship/src/model/CellStatus.java
+++ prog3-battleship/src/model/CellStatus.java
@@ -1,5 +1,22 @@
 package model;
 
+/**
+ * @author Rubén Del Castillo Fuentes 48786827D
+ * Aqui controlamos los estados de cada casilla
+ */
 public enum CellStatus {
-	WATER, HIT, DESTROYED;
+	/**
+	 * Estado Agua
+	 */
+	WATER, 
+	
+	/**
+	 * Estado Hit
+	 */
+	HIT, 
+	
+	/**
+	 * Estado Detroyed
+	 */
+	DESTROYED;
 }
