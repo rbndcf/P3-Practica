23b9577209281522ee560784ff0ff1e21f39d714
RubénDCF <71841727+rvbendcf@users.noreply.github.com>
2020-11-02 01:26:58 +0100

    CoordinateFactory.java v1.0

--- prog3-battleship/src/model/CoordinateFactory.java
+++ prog3-battleship/src/model/CoordinateFactory.java
@@ -1,5 +1,17 @@
 package model;
 
-public class CoordinateFactory {
+import model.aircraft.*;
+import model.ship.*;
 
+public class CoordinateFactory{
+	public static Coordinate createCoordinate(int... coords) {
+		if(coords.length == 2)
+			return new Coordinate2D(coords[0], coords[1]);
+		
+		else if(coords.length == 3)
+			return new Coordinate3D(coords[0], coords[1], coords[2]);
+		
+		else
+			throw new IllegalArgumentException();
+	}
 }
