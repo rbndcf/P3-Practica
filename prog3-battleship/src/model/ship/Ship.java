422d7ef5c522656c6cf955a9a1cbff2e321629cc
RubénDCF <71841727+rvbendcf@users.noreply.github.com>
2020-11-06 23:28:27 +0100

    Ship.java v3.0j

--- prog3-battleship/src/model/ship/Ship.java
+++ prog3-battleship/src/model/ship/Ship.java
@@ -5,9 +5,7 @@ import model.*;
 /**
  * @author Rubén Del Castillo Fuentes 48786827D
  * 
- * Esta clase la utilizaremos para controlar los valores de cada barco, crearlos, posicionarlos, saber su posición
- * y su estado (No hit, hit o shot down), saber su orientación y su forma (shape), además de poder obtener cada 
- * uno de sus elementos como symbol, name, posición, etc.
+ * Esta clase la utilizaremos para crear Ships
  */
 public abstract class Ship extends Craft {
 	/**
