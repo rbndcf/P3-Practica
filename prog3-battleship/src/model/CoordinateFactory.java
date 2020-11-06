c42f43f796599f4dd984a94314c182bdf4be4f08
RubénDCF <71841727+rvbendcf@users.noreply.github.com>
2020-11-06 16:51:54 +0100

    CoordinateFactory.java v3.0j

--- prog3-battleship/src/model/CoordinateFactory.java
+++ prog3-battleship/src/model/CoordinateFactory.java
@@ -3,10 +3,16 @@ package model;
 import model.aircraft.*;
 import model.ship.*;
 
+/**
+ * @author Rubén Del Castillo Fuentes 48786827D
+ *
+ * Esta clase la utilizaremos para poder crear coordenadas, ya sean 2D o 3D, sin tener que crear una en específico
+ */
 public class CoordinateFactory{
 	/**
-	 * @param coords Coordenadas
+	 * @param coords componentes de la coordenada a crear
 	 * @return Coordinate2D o Coordinate3D
+	 * @throws IllegalArgumentException si faltan/sobran componentes para la coordenada
 	 * Recibe varias coordenadas y devuelve Coordinate2D o Coordinate3D según la cantidad de estas. O una Excepcion si son menos de 2 o mas de 3 coordenadas
 	 */
 	public static Coordinate createCoordinate(int... coords) {
