02c867ea34954fd58aca3be1bf90d2903e749f62
RubénDCF <71841727+rvbendcf@users.noreply.github.com>
2020-11-06 23:15:12 +0100

    Aircraft.java v3.0j

--- prog3-battleship/src/model/aircraft/Aircraft.java
+++ prog3-battleship/src/model/aircraft/Aircraft.java
@@ -2,7 +2,18 @@ package model.aircraft;
 
 import model.*;
 
+/**
+ * @author Rubén Del Castillo Fuentes 48786827D
+ * 
+ * Esta clase la utilizaremos para crear Aircrafts
+ */
 public abstract class Aircraft extends Craft{
+	/**
+	 * @param o Orientación
+	 * @param s Símbolo
+	 * @param n Nombre
+	 * Constructor por parámetros
+	 */
 	public Aircraft(Orientation o, char s, String n) {
 		super(o, s, n);
 	}
