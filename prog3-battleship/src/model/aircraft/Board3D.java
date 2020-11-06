564ded0e22221f42155bb428b19ab5701d0af560
RubénDCF <71841727+rvbendcf@users.noreply.github.com>
2020-11-06 23:19:51 +0100

    Board3D.java v3.1j

--- prog3-battleship/src/model/aircraft/Board3D.java
+++ prog3-battleship/src/model/aircraft/Board3D.java
@@ -2,11 +2,28 @@ package model.aircraft;
 
 import model.*;
 
+/**
+ * 
+ * @author Rubén Del Castillo Fuentes 48786827D
+ *
+ * Esta clase la utilizaremos para crear Boards de 3 dimensiones, a parte de comprobar las coordenadas de dicho board
+ * y hacer el show de cada coordenada, separando cada altura con el Board_SEPARATOR de la superclase
+ */
 public class Board3D extends Board{
+	/**
+	 * @param size del board
+	 * Constructor por parámetros
+	 */
 	public Board3D(int size) {
 		super(size);
 	}
 	
+	/**
+	 * @param c Coordenada a checkear
+	 * @throws IllegalArgumentException cuando la coordenada c no es una coordenada 3D
+	 * @return true si esta dentro, false si no
+	 * Recibe una coordenada y devuelve true o false según esté dentro del board o no
+	 */
 	@Override
 	public boolean checkCoordinate(Coordinate c) {
 		if(c instanceof Coordinate3D)
@@ -18,6 +35,12 @@ public class Board3D extends Board{
 			throw new IllegalArgumentException();
 	}
 	
+	/**
+	 * @param unveil
+	 * @return estado de la coordenada
+	 * Recibe si se quieren mostrar todas las casillas o no, en caso de que si se quieran ver se mostrarán todas las coordenadas del board, en caso
+	 * de que no se quieran ver todas solo se mostraran las casillas que hayan sido vistas anteriormente
+	 */
 	@Override
 	public String show(boolean unveil) {
 		StringBuilder sb = new StringBuilder();
