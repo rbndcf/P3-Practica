8b56c157cf3d4503f0c685d6834f5569bfa91adc
RubénDCF <71841727+rvbendcf@users.noreply.github.com>
2020-11-06 15:35:26 +0100

    FighterPreTest.java v3.0

--- prog3-battleship/aluTests/model/aircraft/FighterPreTest.java
+++ prog3-battleship/aluTests/model/aircraft/FighterPreTest.java
@@ -3,6 +3,7 @@ package model.aircraft;
 import static org.junit.Assert.*;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
@@ -11,8 +12,8 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import model.Coordinate;
+import model.CoordinateFactory;
 import model.Orientation;
-import model.ship.Coordinate2D;
 
 public class FighterPreTest {
 	Aircraft fighterN, fighterE, fighterS, fighterW;
@@ -105,13 +106,15 @@ public class FighterPreTest {
 				assertEquals(shape[i][j],shapeAux[i][j]);
 	}
 
-	//TODO
 	/* Comprueba que las orientaciones de los Fighter creados en el setUp son 
 	 * correctas.
 	 */
 	@Test
 	public void testGetOrientation() {
-		fail("Realiza el test");
+		assertEquals("Error 1 en testGetOrientation", Orientation.NORTH, fighterN.getOrientation());
+		assertEquals("Error 2 en testGetOrientation", Orientation.EAST, fighterE.getOrientation());
+		assertEquals("Error 3 en testGetOrientation", Orientation.SOUTH, fighterS.getOrientation());
+		assertEquals("Error 4 en testGetOrientation", Orientation.WEST, fighterW.getOrientation());
 	}
 
 	@Test
@@ -119,22 +122,40 @@ public class FighterPreTest {
 		assertEquals('⇄', fighterN.getSymbol());
 	}
 
-	//TODO
 	/* Se comprueba que las posiciones absolutas para la orientación NORTH a partir de
 	 * una Coordinate son correctas.
 	 */
 	@Test
 	public void testGetAbsolutePositionsNorth() {
-		fail("Realiza el test");
+		Set<Coordinate> esperades00 = new HashSet<Coordinate> ();
+		
+		esperades00.add(CoordinateFactory.createCoordinate(new int[] {2, 1, 5}));
+		esperades00.add(CoordinateFactory.createCoordinate(new int[] {2, 2, 5}));
+		esperades00.add(CoordinateFactory.createCoordinate(new int[] {2, 3, 5}));
+		esperades00.add(CoordinateFactory.createCoordinate(new int[] {2, 4, 5}));
+		esperades00.add(CoordinateFactory.createCoordinate(new int[] {1, 2, 5}));
+		esperades00.add(CoordinateFactory.createCoordinate(new int[] {3, 2, 5}));	
+		assertEquals("Error 1 en testGetAbsolutePositionNorth", esperades00, fighterN.getAbsolutePositions(CoordinateFactory.createCoordinate(new int[] {0, 0, 5})));
+		
+		Set<Coordinate> esperades55 = new HashSet<Coordinate>();
+		esperades55.add(CoordinateFactory.createCoordinate(new int[] {7, 6}));
+		esperades55.add(CoordinateFactory.createCoordinate(new int[] {7, 7}));
+		esperades55.add(CoordinateFactory.createCoordinate(new int[] {7, 8}));
+		esperades55.add(CoordinateFactory.createCoordinate(new int[] {7, 9}));
+		esperades55.add(CoordinateFactory.createCoordinate(new int[] {6, 7}));
+		esperades55.add(CoordinateFactory.createCoordinate(new int[] {8, 7}));
+		assertEquals("Error 2 en testGetAbsolutePositionsNorth", esperades55, fighterN.getAbsolutePositions(CoordinateFactory.createCoordinate(new int[] {5, 5})));
 	}
 
-	//TODO
 	/* Comprueba que toString() para cada Fighter creado en el setUp coincide con 
 	 * los correspondientes strings creados en setUpBeforeClass()
 	 */
 	@Test
 	public void testToString() {
-		fail("Realiza el test");
+		assertEquals("E1 en testToString", sNorth, fighterN.toString());
+		assertEquals("E2 en testToString", sEast, fighterE.toString());
+		assertEquals("E3 en testToString", sSouth, fighterS.toString());
+		assertEquals("E4 en testToString", sWest, fighterW.toString());
 	}
 
 }
