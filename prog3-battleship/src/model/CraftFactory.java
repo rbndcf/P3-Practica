8b473e34842dc6034a474adf288f42e4418a4cd6
RubénDCF <71841727+rvbendcf@users.noreply.github.com>
2020-11-29 19:44:56 +0100

    CraftFactory.java v4.0

---
+++ prog3-battleship/src/model/CraftFactory.java
@@ -0,0 +1,19 @@
+package model;
+
+import model.aircraft.*;
+import model.ship.*;
+
+public class CraftFactory {
+	Craft createCraft(String type, Orientation orientation) {
+		switch(type) {
+			case "Bomber": 		return new Bomber(orientation);
+			case "Fighter":		return new Fighter(orientation);
+			case "Transport":	return new Transport(orientation);
+			case "Battleship":	return new Battleship(orientation);
+			case "Carrier":		return new Carrier(orientation);
+			case "Cruiser": 	return new Cruiser(orientation);
+			case "Destroyer":	return new Destroyer(orientation);
+			default: 			return null;
+		}
+	}
+}
