5abb71585a6ec48c761d1d09cf9d436a09b9da60
RubénDCF <71841727+rvbendcf@users.noreply.github.com>
2020-11-29 21:30:59 +0100

    IPlayer.java v4.0

--- prog3-battleship/src/model/io/IPlayer.java
+++ prog3-battleship/src/model/io/IPlayer.java
@@ -1,9 +1,13 @@
 package model.io;
 
 import model.*;
+import model.exceptions.InvalidCoordinateException;
+import model.exceptions.NextToAnotherCraftException;
+import model.exceptions.OccupiedCoordinateException;
+import model.exceptions.io.BattleshipIOException;
 
 public interface IPlayer {
 	public String getName();
-	public void putCrafts(Board b);
+	public void putCrafts(Board b) throws BattleshipIOException, InvalidCoordinateException, OccupiedCoordinateException, NextToAnotherCraftException;
 	public Coordinate nextShoot(Board b);
 }
