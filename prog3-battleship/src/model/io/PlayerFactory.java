95218718fa5d80c8675a38ceb3405e3ed49b1a62
RubénDCF <71841727+rvbendcf@users.noreply.github.com>
2020-12-03 21:13:12 +0100

    PlayerFactory.java v4.1

--- prog3-battleship/src/model/io/PlayerFactory.java
+++ prog3-battleship/src/model/io/PlayerFactory.java
@@ -6,13 +6,8 @@ import model.exceptions.io.*;
 public class PlayerFactory {
 	public static IPlayer createPlayer(String name, String s) throws BattleshipIOException {
 		if(s.contains(".") || s.contains("\\") || s.contains("/")){
-			try {
 				BufferedReader br = new BufferedReader(new StringReader(s));
 				return new PlayerFile(name, br);
-			}
-			catch(Exception e) {
-				throw new BattleshipIOException("No se ha podido leer el fichero " + s);
-			}
 		}
 		
 		else if(isLong(s)) {
