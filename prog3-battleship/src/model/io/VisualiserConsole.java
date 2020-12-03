4cea69872351df53840c30dec9e92770173be115
RubénDCF <71841727+rvbendcf@users.noreply.github.com>
2020-12-03 16:18:14 +0100

    VisualiserConsole.java v4.0

--- prog3-battleship/src/model/io/VisualiserConsole.java
+++ prog3-battleship/src/model/io/VisualiserConsole.java
@@ -1,13 +1,14 @@
 package model.io;
 
 import java.util.Objects;
+import model.*;
 
 public class VisualiserConsole implements IVisualiser{
 	private Game game;
 	
 	public VisualiserConsole(Game g){
 		Objects.requireNonNull(g);
-		game = new Game(g);
+		game = g;
 	}
 	
 	public void show() {
