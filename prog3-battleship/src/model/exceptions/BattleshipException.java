93536ad7a318ee86ac20876f7e3ec62748b04341
RubénDCF <71841727+rvbendcf@users.noreply.github.com>
2020-12-06 16:16:36 +0100

    BattleshipException.java v4.0J

--- prog3-battleship/src/model/exceptions/BattleshipException.java
+++ prog3-battleship/src/model/exceptions/BattleshipException.java
@@ -2,8 +2,7 @@ package model.exceptions;
 
 /**
  * @author Rubén Del Castillo Fuentes 48786827D
- *
- * Esta clase la utilizaremos como Excepción general para los problemas que ocurran relacionado con las coordenadas
+ * Esta clase la utilizaremos como excepción general tanto para Excepciones relacionadas con CoordinateException como con BattleshipIOException
  */
 @SuppressWarnings("serial")
 public abstract class BattleshipException extends Exception{}
