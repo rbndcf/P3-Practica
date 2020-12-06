6fb284acf323a8d8dec2fc4c27164da12d8ef15f
RubénDCF <71841727+rvbendcf@users.noreply.github.com>
2020-12-06 17:55:23 +0100

    VisualiserFactory.java v4.1J

--- prog3-battleship/src/model/io/VisualiserFactory.java
+++ prog3-battleship/src/model/io/VisualiserFactory.java
@@ -2,7 +2,19 @@ package model.io;
 
 import model.*;
 
+/**
+ * @author Rubén Del Castillo Fuentes 48786827D
+ * 
+ * Esta clase la utilizaremos como factoria para crear Visualisers tanto de Consola como de GIF
+ */
 public class VisualiserFactory {
+	/**
+	 * @param n tipo de Visualiser a crear
+	 * @param g game que se va a utilizar
+	 * @return tipo de visualiser especificado
+	 * Recibe una cadena donde esta el tipo de visualiser que se quiere crear, en caso de ser Console o GIF se devuelve uno de esos dos, en caso de que
+	 * no sea ninguno de esos dos tipos de visualiser devuelve null.
+	 */
 	public static IVisualiser createVisualiser(String n, Game g) {
 		if(n == "Console")
 			return new VisualiserConsole(g);
