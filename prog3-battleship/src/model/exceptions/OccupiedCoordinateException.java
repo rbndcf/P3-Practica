28d6c9a53fc12c12c16382470c3c99d1cb83e87b
RubénDCF <71841727+rvbendcf@users.noreply.github.com>
2020-12-06 16:19:51 +0100

    OccupiedCoordinateException.java v4.0J

--- prog3-battleship/src/model/exceptions/OccupiedCoordinateException.java
+++ prog3-battleship/src/model/exceptions/OccupiedCoordinateException.java
@@ -12,7 +12,7 @@ import model.*;
 public class OccupiedCoordinateException extends CoordinateException{
 	/**
 	 * @param c Coordenada que causa el problema
-	 * Recibe la coordenada que causa el problema y la guarda en BattleshipException
+	 * Recibe la coordenada que causa el problema y la guarda en CoordinateException
 	 */
 	public OccupiedCoordinateException(Coordinate c) {
 		super(c);
