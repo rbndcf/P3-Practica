44571145848eede4bb6d0af1e2c8d567272d6c1d
RubénDCF <71841727+rvbendcf@users.noreply.github.com>
2020-12-06 16:00:21 +0100

    CraftFactory.java v4.1J

--- prog3-battleship/src/model/CraftFactory.java
+++ prog3-battleship/src/model/CraftFactory.java
@@ -3,7 +3,17 @@ package model;
 import model.aircraft.*;
 import model.ship.*;
 
+/**
+ * @author Rubén Del Castillo Fuentes 48786827D
+ * 
+ * Esta clase la utilizaremos como factoria para crear crafts de cualquier tipo, aviones o barcos
+ */
 public class CraftFactory {
+	/**
+	 * @param type Tipo del craft que vamos a crear
+	 * @param orientation Orientación del craft que vamos a crear
+	 * @return el craft creado
+	 */
 	public static Craft createCraft(String type, Orientation orientation) {
 		switch(type) {
 			case "Bomber": 		return new Bomber(orientation);
