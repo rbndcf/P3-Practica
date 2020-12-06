2abe856bea1bacdc6a35081578509ba119536172
RubénDCF <71841727+rvbendcf@users.noreply.github.com>
2020-12-06 17:54:24 +0100

    IVisualiser.java v4.0J

--- prog3-battleship/src/model/io/IVisualiser.java
+++ prog3-battleship/src/model/io/IVisualiser.java
@@ -1,6 +1,17 @@
 package model.io;
 
+/**
+ * @author Rubén Del Castillo Fuentes 48786827D
+ * 
+ * Interfaz que utilizaremos para crear los diferentes Visualisers
+ */
 public interface IVisualiser {
+	/**
+	 * Muetra como está la partida en ese momento
+	 */
 	void show();
+	/**
+	 * Cierra y guarda el fichero GIF
+	 */
 	void close();
 }
