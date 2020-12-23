11fb302bf017254856b96667c1f11d52078e1b71
ruben <ruben@Ruben-Laptop>
2020-12-23 13:51:11 +0100

    CraftScore.java v5.0

---
+++ prog3-battleship/src/model/score/CraftScore.java
@@ -0,0 +1,27 @@
+package model.score;
+
+import model.*;
+import model.io.*;
+
+/**
+ * @author Rubén Del Castillo Fuentes 48786827D
+ *
+ * Esta clase la utilizaremos para representar un score basado en Craft y aumenta el score con el valor del Craft
+ */
+public class CraftScore extends Score<Craft>{
+	/**
+	 * @param player Jugador a guardar
+	 * Constructor por parámetros
+	 */
+	public CraftScore(IPlayer player) {
+		super(player);
+	}
+	
+	/**
+	 * @param sc Craft del que se saca el valor
+	 * Aumenta el valor del parámetro score con el valor del Craft recibido
+	 */
+	public void score(Craft sc) {
+		score += sc.getValue();
+	}
+}
